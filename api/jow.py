1929cfed65bdaa8c870880c58abd01cf459e2d33
romeji <lopes.jerome21@gmail.com>
2026-06-16 09:37:51 +0200

    Update jow.py

--- api/jow.py
+++ api/jow.py
@@ -232,13 +232,13 @@ class handler(BaseHTTPRequestHandler):
     def _force_png(self, url):
         if not url:
             return None
-    return url.rsplit(".", 1)[0] + ".png" if "." in url else url + ".png"
+        return url.rsplit(".", 1)[0] + ".png" if "." in url else url + ".png"
 
 
     def _force_jpg(self, url):
         if not url:
             return None
-    return url.rsplit(".", 1)[0] + ".jpg" if "." in url else url + ".jpg"
+        return url.rsplit(".", 1)[0] + ".jpg" if "." in url else url + ".jpg"
 
     def _build_image_url(self, recipe, details):
 
